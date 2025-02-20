9695386e85a6ad8292fc992f47c6e9599711111f
Houtmen <houtmen.elmo@gmail.com>
2025-02-20 16:38:18 +0100

    test

---
+++ Classe.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+class maturite :
+    def __init__(self,val_date:str,mat_date:str,convention : int):
+        # Pour convention je vais mettre des fichiers qui seront lié a la convention (il en faut 4)
+        pass
+
+class taux :
+    def __init__(self):
+        pass
+    # obj : Courbe de taxux
+    # avec des methodes d'interpo (voir debut)
+
+class produits :
+    def __init__(self):
+        pass
+    # obj : pricer un ZC Bond
+    # obj2 : pricer un Fixed Rate bond
+
+
+
