e75c1c5d553414445d40d2d7dd95686ab35a6a38
Houtmen <houtmen.elmo@gmail.com>
2025-02-20 16:26:29 +0100

    Debut du projet

---
+++ debut.py
@@ -0,0 +1,21 @@
+# Premier cours de produit structué (1/6)
+# C'est un projet PYTHON purement écrit 2 semaine apres la fin du dernier cours
+# Sujet de Pricing ou on peut pricer tout quoi
+# B(t+delta_t)  = B(t)*exp(delta_t*r)
+# B(t+delta_t)  = B(t) * DF
+
+
+# Pour une courbe de taux il vaut mieux travailler avec une interpo spline cubique qu'une interpo cuboc
+# Sinon il existe le modele : Nelson Siegel
+# r(t) = B0 + B1*(1-exp(lambda*delta_t)/(lambda*t)) + B2*((1-exp(-lambda*delta_t))/(lamnda*t)-exp(-lambda*t))
+
+# Mais en realite il vaudrait mieux utliser SVENSON pour interpoler
+
+# On peut installer GITHUB DESKTOP et il faut le mapper au compte
+# Une fois installer on a tous les projets existant
+# Il faut vraiment apprendre GITHUB askip les entreprises aime bien
+
+
+# Debut du projet :
+
+
