17aef62da03dccc8cdef5f6c14a696d5bbc7aa36
Houtmen <houtmen.elmo@gmail.com>
2025-02-27 16:47:06 +0100

    h

--- calibration_Nelson_Siegel.py
+++ calibration_Nelson_Siegel.py
@@ -1,25 +1,32 @@
 import numpy as np
 import pandas as pd
 
-def nelson_siegel(t,beta0,beta1,beta2,coef,lambda_coeff):
-    return beta0 +  beta1*(1-np.exp(-t/lambda_coeff))/(t/lambda_coeff) +beta2*(1-np.exp(-t/lambda_coeff)/(t/lambda_coeff)-np.exp(-t/lambda_coeff))
+def nelson_siegel(
+    t:float,
+    beta0_coeff:float,
+    beta1_coeff:float,
+    beta2_coeff:float,
+    lambda_coeff:float
+    ):
+    return beta0_coeff + \
+        beta1_coeff * (1 - np.exp(-t / lambda_coeff)) / (t / lambda_coeff) + \
+        beta2_coeff * ((1 - np.exp(-t / lambda_coeff)) / (t / lambda_coeff) - \
+                            np.exp(-t / lambda_coeff))
+
 
 # Calibration
-df = pd.read_csv('RateCurve.csv',sep=";")
+df=pd.read_csv('RateCurve.csv', sep=";")
 def convert_mat(pillar):
-    if "M" in pillar :
-        return int(pillar.replace("M",""))/12
-    if "Y" in pillar :
-        return int(pillar.replace("M",""))
-    raise ValueError("Unknown format") # Permet de renvoyer un beau msg d'erreur
-df["maturity"] = df["Pillar"].map(convert_mat)  # Grosse difference de puissance entre map et Apply (donc tjr uitlier map)
-# Methode np.vectorize on lui donne une fct et apres les arguments et np.vectorize va vectoriser la fonction  a toute vitesse
-maturities = list(df["maturity"])
-rates = list(df["Rate"])
+    if "M" in pillar:
+        return int(pillar.replace("M", "")) / 12
+    if "Y" in pillar:
+        return int(pillar.replace("Y", ""))
+    raise ValueError("Unknown format")
+df["maturity"]=df["Pillar"].map(convert_mat)
+
+maturities=list(df["maturity"])
+rates=list(df["Rate"])
 
 from scipy.optimize import curve_fit
-popt,_= curve_fit(nelson_siegel,maturities,rates)
-
-
-
-
+popt, _=curve_fit(nelson_siegel, maturities, rates)
+print(popt)
--- debut.py
+++ debut.py
@@ -28,6 +28,48 @@
 # Puis faire : pip freeze > requirements.txt  Ca c'est bien ca permet de faire un fichier txt avec les bonnes libraires
 # La personne qui veut l'utliser doit faire : pip install -r requirements.txt
 
+####### 2 eme partie du cours 2 : #######
+# Attention difference entre Obligation Revisable et Obligation Variable
+# Le coupon d'une revisable c'est entre EURIBOR/LIBOR
+# Le coupon d'un variable est connu a la fin et c'et sur un OIS souvent.
+
+# Coupon =  (alpha*I + m) * D_i
+# Alpha c'est la marge multiplicative
+# I c'est la cotation de l'indice
+# m c'est la marge additionelle
+# D_i c'est le taux
+
+# 2) Taux forward
+# Soit on utilise les taux composé = F(t1,t2) = ((1+r2)^t_2)/(1+r1)^t_1)^(1/(t2-t1)) - 1
+
+#3) Taux continu
+# taux = (r2*t2 - r1*t1)/(t2-t1)
+
+
+#4) Swap IRS
+
+
+
+# Construciton du code python
+# Le projet serar dans l'idee d'un tableau de bord de pricing
+# Class Utils -> Maturity
+# -> RATE (courbe de taux) -> il faudra sortir le taux sport et les taux fwd
+#
+# Clas Produit :
+# Zc
+# Portfolio : Fixed Income, Float Income, Swap, Currency swap
+#
+
+# La semaine pro on va lancer Streamit qui va permette de faire des tableau de bord
+
+
+
+
+
+
+
+
+
 
 
 
